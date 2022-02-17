299222e3ad4909c356f3c76fc39cae5b9dc18252
saimon169 <saimonmaharjan091@gmail.com>
2022-02-16 18:19:07 -0800

    Area of Rectangle

---
+++ AreaOfRectangle.java
@@ -0,0 +1,7 @@
+class AreaOfRectangle{
+    public static void main(String[] args){
+        int length=45,breadth=10,area;
+        area=length*breadth;fd
+        System.out.println("The area of reactangle is: " +area);
+    }
+}
